eb673c6aedfd78bd69b4d35228589769e8a3eb43
Lev <Lev.Shisterov@gmail.com>
2012-03-10 04:51:07 +0000

    Added version information to client.dll.

---
+++ cl_dll/version.cpp
@@ -0,0 +1,11 @@
+/* 
+*  Version declaration dependency file
+*
+*/
+
+//
+// This file needed just to add the dependency on version.h and appversion.h
+//
+#include "version.h"
+#include "appversion.h"
+
---
+++ cl_dll/version.h
@@ -0,0 +1,17 @@
+/* 
+*  Version declaration header
+*
+*/
+
+#ifndef __CLDLL__VERSION_H__
+#define __CLDLL__VERSION_H__
+
+//
+// Define real app version here
+//
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+//#define VERSION_MAINTENANCE 0
+
+#endif //__CLDLL__VERSION_H__
