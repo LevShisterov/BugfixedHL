3d42b03a65f521cdd72546e9f8cf39658ecdc423
Lev <Lev.Shisterov@gmail.com>
2012-12-21 15:00:57 +0000

    Multiple deployment paths support with .pdb copying.

--- dlls/msvc8/PostBuild.bat
+++ dlls/msvc8/PostBuild.bat
@@ -5,19 +5,27 @@
 :: I.e. PublishPath.txt should contain one line with a folder path
 ::
 
-SET targetfile=%~1
-SET projectdir=%~2
+SET targetDir=%~1
+SET targetName=%~2
+SET targetExt=%~3
+SET projectDir=%~4
 SET destination=
 
-IF NOT EXIST "%projectdir%\PublishPath.txt" (
-	ECHO 	No deployment path specified.
+IF NOT EXIST "%projectDir%\PublishPath.txt" (
+	ECHO 	No deployment path specified. Create PublishPath.txt near PostBuild.bat with paths on separate lines for auto deployment.
 	exit /B 0
 )
 
-SET /p destination= <"%projectdir%\PublishPath.txt"
+FOR /f "tokens=* delims= usebackq" %%a IN ("%projectDir%\PublishPath.txt") DO (
+	IF NOT "%%a" == "" (
+		copy /Y "%targetDir%%targetName%%targetExt%" "%%a"
+		IF EXIST "%targetDir%%targetName%.pdb" (
+			copy /Y "%targetDir%%targetName%.pdb" "%%a"
+		)
+	)
+)
+
 IF "%destination%" == "" (
 	ECHO 	No deployment path specified.
 	exit /B 0
 )
-
-copy /Y "%targetfile%" "%destination%"
