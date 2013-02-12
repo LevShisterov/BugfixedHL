dd19caec44ef79a00ce246df4cf8656b5e2e3ce9
Lev <Lev.Shisterov@gmail.com>
2013-02-12 06:18:35 +0000

    #1: Added server-side cvar mp_notify_player_status to allow selecting notifications about join/leave/spectate.

--- dlls/game.cpp
+++ dlls/game.cpp
@@ -40,6 +40,7 @@ cvar_t	defaultteam = {"mp_defaultteam","0" };
 cvar_t	allowmonsters={"mp_allowmonsters","0", FCVAR_SERVER };
 
 cvar_t  mp_chattime = {"mp_chattime","10", FCVAR_SERVER };
+cvar_t  mp_notify_player_status = {"mp_notify_player_status","7"};	// Notifications about join/leave/spectate
 
 // Engine Cvars
 cvar_t 	*g_psv_gravity = NULL;
@@ -480,7 +481,7 @@ void GameDLLInit( void )
 	CVAR_REGISTER (&allowmonsters);
 
 	CVAR_REGISTER (&mp_chattime);
-
+	CVAR_REGISTER (&mp_notify_player_status);
 
 	// REGISTER CVARS FOR SKILL LEVEL STUFF
 
