31027584db561bc08258d58eb9e194e1c38c635d
Lev <Lev.Shisterov@gmail.com>
2013-05-08 13:18:46 +0000

    Fixed health value clamping again and finally.

--- dlls/client.cpp
+++ dlls/client.cpp
@@ -1545,8 +1545,8 @@ void UpdateClientData ( const struct edict_s *ent, int sendweapons, struct clien
 		cd->health		= 0.0;
 	else if (ent->v.health <= 1.0)
 		cd->health		= 1.0;
-	else if (ent->v.health > 0x7FFFFFFF)
-		cd->health		= 0x7FFFFFFF;
+	else if ((unsigned int)ent->v.health > 0x7FFF0000)
+		cd->health		= 0x7FFF0000;
 	else
 		cd->health		= ent->v.health;
 
