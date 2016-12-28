0b70df97d2a75cb93e6300cf9e30a9b3b7a3e412
Lev <Lev.Shisterov@gmail.com>
2016-12-29 04:51:23 +0600

    Fix for appversion.sh

--- dlls/appversion.sh
+++ dlls/appversion.sh
@@ -2,7 +2,8 @@
 
 	# Get old version information from appversion.h
 	if [ -e ./appversion.h ]; then
-		#old_version=$(cat ./appversion.h | grep -i '#define APP_VERSION' | sed -e 's/#define APP_VERSION \(.*\)/\1/i')
+		old_version=$(cat ./appversion.h | grep -i '#define APP_VERSION ' | sed -e 's/#define APP_VERSION \(.*\)/\1/i')
+		# fix MC coloring '
 		if [ $? -ne 0 ]; then
 			old_version=""
 		fi
@@ -17,20 +18,19 @@
 	new_version=${new_version/-dirty/+m}
 	new_version=${new_version/-g/+}
 	new_version=${new_version/-/.}
-	echo $new_version
 
 	if [ "$new_version" != "$old_version" ]; then
-		echo Updating appversion.h, old version "$old_version", new version "$new_version".
+		echo "Updating appversion.h, old version \"$old_version\", new version \"$new_version\"."
 
 		echo '#ifndef __APPVERSION_H__' > appversion.h
 		echo '#define __APPVERSION_H__' >> appversion.h
 		echo '' >> appversion.h
 
 		echo -n '#define APP_VERSION ' >> appversion.h
-		echo $new_version >> appversion.h
+		echo "\"$new_version\"" >> appversion.h
 
 		echo -n '#define APP_VERSION_DATE ' >> appversion.h
-		echo $git_date >> appversion.h
+		echo "\"$git_date\"" >> appversion.h
 
 		echo '' >> appversion.h
 		echo '#endif //__APPVERSION_H__' >> appversion.h
