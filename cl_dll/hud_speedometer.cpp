719ed8e5cd45d310db10a79e1b5b6b6e7ef3ff72
rtxa <usertxa@gmail.com>
2019-02-11 20:28:25 -0300

    Added dimming for speedometer

--- cl_dll/hud_speedometer.cpp
+++ cl_dll/hud_speedometer.cpp
@@ -28,9 +28,30 @@ int CHudSpeedometer::Draw(float time)
 	if (m_pCvarSpeedometer->value == 0.0f)
 		return 0;
 
+	if (m_iOldSpeed != m_iSpeed)
+		m_fFade = FADE_TIME;
+
 	int r, g, b;
+	float a;
+
+	if (gHUD.m_pCvarDim->value == 0)
+		a = MIN_ALPHA + ALPHA_AMMO_MAX;
+	else if (m_fFade > 0)
+	{
+		// Fade the health number back to dim
+		m_fFade -= (gHUD.m_flTimeDelta * 20);
+		if (m_fFade <= 0)
+			m_fFade = 0;
+		a = MIN_ALPHA + (m_fFade / FADE_TIME) * ALPHA_AMMO_FLASH;
+	} else
+		a = MIN_ALPHA;
+
+	a *= gHUD.GetHudTransparency();
 	gHUD.GetHudColor(0, 0, r, g, b);
+	ScaleColors(r, g, b, a);
 	gHUD.DrawHudNumberCentered(ScreenWidth / 2, ScreenHeight - gHUD.m_iFontHeight - gHUD.m_iFontHeight / 2, m_iSpeed, r, g, b);
+
+	m_iOldSpeed = m_iSpeed;
 
 	return 0;
 }
--- cl_dll/hud_speedometer.h
+++ cl_dll/hud_speedometer.h
@@ -10,7 +10,9 @@ public:
 	void UpdateSpeed(const float velocity[2]);
 
 private:
+	int m_iOldSpeed;
 	int m_iSpeed;
+	float m_fFade;
 	cvar_t *m_pCvarSpeedometer;
 };
 
